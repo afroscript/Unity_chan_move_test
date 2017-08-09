ee4ed1c94497eebda443bcbe65c612a5f434f25c
afroscript <afroscript.10@gmail.com>
2017-08-09 20:25:54 +0900

    initial

---
+++ Assets/App/Script/SampleSpeachToText.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using IBM.Watson.DeveloperCloud.Services.SpeechToText.v1;
+
+
+public class SampleSpeachToText : MonoBehaviour {
+
+	[SerializeField]
+	private AudioClip m_AudioClip = new AudioClip();
+	private SpeechToText m_SpeechToText = new SpeechToText();
+
+	IEnumerator Start()
+	{
+		// 音声をマイクから3秒間取得
+		Debug.Log ("Start record");
+		var audioSource = GetComponent<AudioSource>();
+		audioSource.clip = Microphone.Start(null, true, 10, 44100);
+		audioSource.loop = false;
+		audioSource.spatialBlend = 0.0f;
+		yield return new WaitForSeconds (3f);
+		Microphone.End (null);
+		Debug.Log ("Finish record");
+
+		// ためしに録音内容を再生してみる
+		audioSource.Play ();
+
+		// 音声をテキストに変換
+		m_SpeechToText.Recognize(audioSource.clip, HandleOnRecognize);
+	}
+		
+	void HandleOnRecognize(SpeechRecognitionEvent result)
+	{
+		if (result != null && result.results.Length > 0)
+		{
+			foreach (var res in result.results)
+			{
+				foreach (var alt in res.alternatives)
+				{
+					string text = alt.transcript;
+					Debug.Log(string.Format("{0} ({1}, {2:0.00})\n", text, res.final ? "Final" : "Interim", alt.confidence));
+				}
+			}
+		}
+	}
+}
